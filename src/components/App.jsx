6ea432112d693362155d87d7c7ddc160b8782254
Volodymyr Romanenko <romanonebe@gmail.com>
2022-07-20 14:14:15 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  countTotalFeedback = () => {};
+
+  countPositiveFeedbackPercentage = () => {};
+
+  render() {
+    return (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        {/* <Section title="">
+           <FeedbackOptions options={} onLeaveFeedback={}></FeedbackOptions>
+          <Statistics good={} neutral={} bad={} total={} positivePercentage={}>
+       
+         </Section> */}
+      </div>
+    );
+  }
+}
+
+export { App };
